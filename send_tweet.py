18e06951ff02971cfc37485dc2e3d0486c1d4f65
Shubhra Tewari <tewaris@whitman.edu>
2018-07-26 12:27:10 -0700

    added if condition to check if jobs were posted in last four hours

--- send_tweet.py
+++ send_tweet.py
@@ -3,33 +3,41 @@ import json
 import boto3
 import os
 
-from twython import Twython
+import tweepy
 from secrets import get_secret
 
-#get API keys
-CONSUMER_KEY = get_secret('TWITTER_CONSUMER_KEY')
-CONSUMER_SECRET = get_secret('TWITTER_CONSUMER_SECRET')
-ACCESS_TOKEN_KEY = get_secret('TWITTER_ACCESS_TOKEN_KEY')
-ACCESS_TOKEN_SECRET = get_secret('TWITTER_ACCESS_TOKEN_SECRET')
-
-# Create the Twython Twitter client using credentials
-twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET,
-                  ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuthHandler(
+    get_secret('TWITTER_CONSUMER_KEY'), 
+    get_secret('TWITTER_CONSUMER_SECRET')
+)
+auth.set_access_token(
+    get_secret('TWITTER_ACCESS_TOKEN_KEY'), 
+    get_secret('TWITTER_ACCESS_TOKEN_SECRET')
+)
+api = tweepy.API(auth)
 
 sqs = boto3.client('sqs')
 
 def send_job_tweet(tweet_text):
     #Sends a tweet to Twitter
-    twitter.update_status(status = tweet_text)
+    api.update_status(status=tweet_text)
 
 def handler(event,context):
     response = sqs.receive_message(
-        QueueUrl='https://sqs.us-east-1.amazonaws.com/075904714953/jobsQueue.fifo'
+        QueueUrl='https://sqs.us-east-1.amazonaws.com/075904714953/jobsQueue.fifo',
+        AttributeNames=['SentTimestamp']
     )
     # sqs.get_queue_url(
     #     QueueName='jobsQueue.fifo',
     #     QueueOwnerAWSAccountId=os.environ['ACCOUNT_ID']
     # ),
-    tweet_text = response['Messages']['Body']
-    send_job_tweet(tweet_text)
+    tweets = []
+    for message in response['Messages']:
+        if message['Attributes']['SentTimestamp'] <= 14400000:
+            tweets.append(message['Body'])
+            for tweet in tweets:
+                send_job_tweet(tweet)
+        else:
+            send_job_tweet('No jobs posted recently, check back in 4 hours!')
+
 
