763663a5d91ddd45eb4d6206443a1442f995d4b7
Shubhra Tewari <tewaris@whitman.edu>
2018-07-25 17:21:09 -0700

    deleted sparrow folder

--- sparrow/creds.py
+++
@@ -1,8 +0,0 @@
-from secrets import get_secret
-
-{ 
-    'consumer_key': get_secret('TWITTER_CONSUMER_KEY'),
-    'consumer_secret': get_secret('TWITTER_CONSUMER_SECRET'),
-    'access_token_key': get_secret('TWITTER_ACCESS_TOKEN_KEY'),
-    'access_token_secret': get_secret('TWITTER_ACCESS_TOKEN_SECRET') 
-}
