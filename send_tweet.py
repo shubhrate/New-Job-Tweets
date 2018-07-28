d40e26ca8ef5872c2ab4818af7116d2f00e629ff
Shubhra Tewari <tewaris@whitman.edu>
2018-07-27 18:11:37 -0700

    added handling for empty queue, get_queue_url function, and max number of messages

--- send_tweet.py
+++ send_tweet.py
@@ -7,37 +7,58 @@ import tweepy
 from secrets import get_secret
 
 auth = tweepy.OAuthHandler(
-    get_secret('TWITTER_CONSUMER_KEY'), 
+    get_secret('TWITTER_CONSUMER_KEY'),
     get_secret('TWITTER_CONSUMER_SECRET')
 )
 auth.set_access_token(
-    get_secret('TWITTER_ACCESS_TOKEN_KEY'), 
+    get_secret('TWITTER_ACCESS_TOKEN_KEY'),
     get_secret('TWITTER_ACCESS_TOKEN_SECRET')
 )
 api = tweepy.API(auth)
 
 sqs = boto3.client('sqs')
+dynamodb = boto3.client('dynamodb')
+
 
 def send_job_tweet(tweet_text):
-    #Sends a tweet to Twitter
+    # Sends a tweet to Twitter
     api.update_status(status=tweet_text)
 
-def handler(event,context):
+
+def handler(event, context):
+    print(event)
     response = sqs.receive_message(
-        QueueUrl='https://sqs.us-east-1.amazonaws.com/075904714953/jobsQueue.fifo',
-        AttributeNames=['SentTimestamp']
+        QueueUrl=sqs.get_queue_url(
+                    QueueName='jobsQueue.fifo',
+                    QueueOwnerAWSAccountId=os.environ['ACCOUNT_ID'])
+                    ['QueueUrl'],
+        AttributeNames=['SentTimestamp'],
+        MaxNumberOfMessages=10
     )
-    # sqs.get_queue_url(
-    #     QueueName='jobsQueue.fifo',
-    #     QueueOwnerAWSAccountId=os.environ['ACCOUNT_ID']
-    # ),
-    tweets = []
-    for message in response['Messages']:
-        if message['Attributes']['SentTimestamp'] <= 14400000:
-            tweets.append(message['Body'])
-            for tweet in tweets:
-                send_job_tweet(tweet)
-        else:
-            send_job_tweet('No jobs posted recently, check back in 4 hours!')
-
-
+    print(response)
+    try:
+        tweets = []
+        for message in response['Messages']:
+            data = json.loads(message['Body'])
+            text = data['Tweet']
+            tweets.append(text)
+            sqs.delete_message(
+                QueueUrl=sqs.get_queue_url(
+                            QueueName='jobsQueue.fifo',
+                            QueueOwnerAWSAccountId=os.environ['ACCOUNT_ID'])
+                            ['QueueUrl'],
+                ReceiptHandle=message['ReceiptHandle']
+            )
+        for tweet in tweets:
+            send_job_tweet(tweet)
+            # dynamodb.update_item(
+            #     TableName='jobsTable',
+            #     Key={
+            #         'job_id': {
+            #             'S': data['Job_ID']
+            #         }
+            #     },
+            #     UpdateExpression='set posted_to_twitter = Yes'
+            # )
+    except KeyError:
+        print('No new jobs!')
