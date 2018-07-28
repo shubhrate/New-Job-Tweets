f495c691b9906d18cf98f889545fd3983aae0e8d
Shubhra Tewari <tewaris@whitman.edu>
2018-07-27 18:12:43 -0700

    added job_id to tweetDict and get_queue_url function

--- streamprocessor.py
+++ streamprocessor.py
@@ -6,17 +6,12 @@ import os
 import random
 
 
-print('Loading function')
-
-
 def lambda_handler(event, context):
     print(event)
     for record in event['Records']:
-        #process every record in event
-        #Check that it is a new record
+        # Check that record is new
         if record['eventName'] == "INSERT":
-            #process data
-            #extract job title, description, apply link, employer, etc.
+            # Extract job title, description, apply link, employer, etc.
             job_title = record['dynamodb']['NewImage']['core_job_title']['S']
             print(f"Job title: {job_title}")
             job_category = record['dynamodb']['NewImage']['core_job_category']['S']
@@ -29,37 +24,30 @@ def lambda_handler(event, context):
             print(f"Job description: {job_description}")
             emp_type = record['dynamodb']['NewImage']['core_job_employment_type']['S']
             print(f"Employment type: {emp_type}")
-            #construct tweet from job info
-                #random list of tweet formats
+            # Construct tweet from random list of tweet formats
             potential_tweets = [
                 f'New job posting!\n{org_name} is looking for a {job_title} in {job_category}.\n{org_name} describes this {emp_type} job as: {job_description}.\nIf you are interested, apply here: {apply_link}',
                 f'Want to get a job at {org_name}?\nWell, {org_name} wants a {emp_type} {job_title} to work in {job_category}.\nThis job is described as: {job_description} and you can apply for it at {apply_link}',
                 f'Latest job posting at {org_name}:\nIf you want to work {emp_type} as a {job_title} in {job_category}, apply at {apply_link}!\nThis job is described by {org_name} as: {job_description}.'
             ]
             tweet = random.choice(potential_tweets)
-            #send it to SQS
-            #print full tweet
             print(f"Full tweet: {tweet}")
-            #create python dictionary with tweet and image
+            # Create python dictionary with tweet and image
             tweetDict = {
                 'Tweet': tweet,
-                'Image': 'https://goo.gl/images/focHyA' 
+                'Image': 'https://goo.gl/images/focHyA',
+                'Job_ID': record['dynamodb']['Keys']['job_id']['S']
             }
-            #store as message for SQS
-            message = json.dumps(tweetDict) #turns into string
-            #Send tweet message to FIFO queue
+            # Store as string message for SQS
+            message = json.dumps(tweetDict)
+            # Send tweet message to FIFO queue
             sqs = boto3.client('sqs')
             response = sqs.send_message(
-                QueueUrl = 'https://sqs.us-east-1.amazonaws.com/075904714953/jobsQueue.fifo',
-
-                # sqs.get_queue_url(
-                #     QueueName='jobsQueue.fifo',
-                #     QueueOwnerAWSAccountId=os.environ['ACCOUNT_ID']
-                # ),
+                QueueUrl=sqs.get_queue_url(
+                            QueueName='jobsQueue.fifo',
+                            QueueOwnerAWSAccountId=os.environ['ACCOUNT_ID'])
+                            ['QueueUrl'],
                 MessageBody=message,
                 MessageGroupId='twitter'
             )
             print(response)
-
-
- 
